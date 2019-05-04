21f670485a13b7cc19353a243d826567f8004966
02aje96 <50267637+02aje96@users.noreply.github.com>
2019-05-04 14:39:03 +0100

    Update MyAddon.lua

--- MyAddon/MyAddon.lua
+++ MyAddon/MyAddon.lua
@@ -1,4 +1,4 @@
--- Marmaite
+-- Marmaites
 local playerName = UnitName("player")
 ChatFrame1:AddMessage('Hi my name is: ' .. playerName .. " my level is " .. UnitLevel("player")+1) 
 function MAOnLoad()
